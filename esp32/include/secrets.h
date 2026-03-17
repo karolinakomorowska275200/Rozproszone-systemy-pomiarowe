74269ebec797f77f595a1b9b036cc601f56dfa20
karolinakomorowska <275200@student.pwr.edu.pl>
2026-03-17 12:09:34 +0100

    lab3

--- esp32/include/secrets.h
+++ esp32/include/secrets.h
@@ -1,2 +1,6 @@
-#define WIFI_SSID "LAB_WIFI"
+#pragma once
+#define WIFI_SSID "esp_lab"
+#define WIFI_PASSWORD "12345678"
 #define MQTT_HOST "156.17.45.150"
+#define MQTT_PORT 1883
+#define MQTT_GROUP "g03"
--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,21 +1,81 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
 #include "secrets.h"
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+String deviceId;
+String topic;
+String generateDeviceIdFromEfuse() {
+uint64_t chipId = ESP.getEfuseMac();
+char id[32];
+snprintf(id, sizeof(id), "esp32-%04X%08X",
+(uint16_t)(chipId >> 32),
+(uint32_t)chipId);
+return String(id);
+}
+void connectWiFi() {
+Serial.print("Laczenie z Wi-Fi: ");
+Serial.println(WIFI_SSID);
+WiFi.mode(WIFI_STA);
+WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+while (WiFi.status() != WL_CONNECTED) {
+delay(500);
+Serial.print(".");
+}
+Serial.println();
+Serial.println("Polaczono z Wi-Fi");
+Serial.print("Adres IP: ");
+Serial.println(WiFi.localIP());
+}
+void connectMQTT() {
+mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+while (!mqttClient.connected()) {
+Serial.print("Laczenie z MQTT...");
+if (mqttClient.connect(deviceId.c_str())) {
+Serial.println("OK");
+} else {
+Serial.print("blad, rc=");
+Serial.print(mqttClient.state());
+Serial.println(" - ponowna proba za 2 s");
+delay(2000);
+}
+}
+}
 
-
-Sring uniqDeviceID();
+void publishMeasurement() {
+StaticJsonDocument<256> doc;
+doc["device_id"] = deviceId;
+doc["sensor"] = "temperature";
+doc["value"] = temperatureRead();
+doc["unit"] = "C";
+doc["ts_ms"] = millis();
+char payload[256];
+serializeJson(doc, payload);
+mqttClient.publish(topic.c_str(), payload);
+Serial.print("Publikacja na topic: ");
+Serial.println(topic);
+Serial.println(payload);
+}
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115.200) ;
+Serial.begin(115200);
+delay(1000);
+deviceId = generateDeviceIdFromEfuse();
+topic = "lab/" + String(MQTT_GROUP) + "/" + deviceId + "/temperature";
+Serial.print("Device ID: ");
+Serial.println(deviceId);
+connectWiFi();
+connectMQTT();
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(5000) ;
-  Serial.println("loop every 5s") ;
+if (WiFi.status() != WL_CONNECTED) {
+connectWiFi();
 }
-
-String uniqDeviceID()(
-  uint64_t chipId = ESP.getEfuseMac();
-  char id[32];
-  sprintf(id,sizeof(id), "esp32-%04X%08X", (uint16_t)(chipId>>32), (uint32_t)chipId);
-)
+if (!mqttClient.connected()) {
+connectMQTT();
+}
+mqttClient.loop();
+publishMeasurement();
+delay(5000);
+}
